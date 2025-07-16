b3957a280e3a98799067190265723b66a00d5ecb
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-16 16:01:18 +0800

    Complete 10_leapYears exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,27 @@
-const leapYears = function() {
+const isCentury = year => { return year%100 === 0}
 
+const leapYears = function(year) {
+	
+	// if(isCentury(year)) {
+	// 	if(year%400 === 0) {
+	// 		return true;
+	// 	} else { 
+	// 		return false;
+	// 	}
+	// } else if (year % 4 === 0) {
+	// 	return true;
+	// } else {
+	// 	return false;
+	// }
+
+	// year divisible by 100 not leap unless divisible by 400 as well
+	// year divisible by 4 is leap
+	
+	if(isCentury(year)) {
+		return year%400 === 0;
+	}
+
+	return year%4 === 0;
 };
 
 // Do not edit below this line
