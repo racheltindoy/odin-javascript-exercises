057377d46ecfc8dc47d7f16f3b3bf239135b46ac
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-16 16:23:44 +0800

    Complete 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let converted = (temp - 32) * 5/9;
+  let result = Math.round(converted * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let converted = (temp * 9/5) + 32;
+  let result = Math.round(converted * 10) / 10;
+
+  return result;
 };
 
 // Do not edit below this line
