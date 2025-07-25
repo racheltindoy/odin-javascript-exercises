95bc3408b6c2cb32a124c7794fc7866b7708420c
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-25 11:54:07 +0800

    finish 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(val1, val2) {
+	return val1 + val2;
 };
 
-const subtract = function() {
-	
+const subtract = function(val1, val2) {
+	return val1 - val2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length === 0) {
+    return 0;
+  } else {
+    return arr.reduce((total, num) => total + num, 0)
+  }
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if(arr.length === 0) {
+    return 0;
+  } else {
+    return arr.reduce((product, num) => product * num)
+  }
+};
+
+const power = function(val, pow) {
+  let arr = [];
+
+  for(let i = 0; i  < pow; i++) {
+    arr.push(val);
+  }
+
+  return arr.reduce((acc, curr) => {
+    return acc *= curr;
+  });
 
 };
 
-const power = function() {
-	
-};
+const factorial = function(val) {
+    if(val == 0) {return 1}
 
-const factorial = function() {
-	
+    let acc = 1;
+
+    for(let i = val; i > 0; i--) {
+      acc *= i;
+    
+    }
+
+    return acc;
 };
 
 // Do not edit below this line
