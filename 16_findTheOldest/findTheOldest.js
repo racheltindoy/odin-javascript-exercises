d5404c0f3eacef0aa38128a4fd2197de04c6a63e
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-25 16:09:56 +0800

    complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	let currentDate = new Date();
+	let currentYear = currentDate.getFullYear();
+	let people2 = [];
 
+	people.forEach(person => {
+		if (!('yearOfDeath' in person)) {
+			person.yearOfDeath = currentYear;
+			people2.push(person);
+		} else {people2.push(person)}
+	});
+
+	const sorted = people2.sort((a, b) => 
+		(b.yearOfDeath - b.yearOfBirth) - 
+		(a.yearOfDeath - a.yearOfBirth)
+		
+	);
+
+	return sorted[0];
 };
 
 // Do not edit below this line
