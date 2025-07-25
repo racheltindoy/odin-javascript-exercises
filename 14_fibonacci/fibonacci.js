10d28233f8bfc5999e404b16c963090973769176
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-25 14:24:28 +0800

    complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	let a = 1;
+	let lastNum = 0;
+	let sum = 0;
+	let arr = [];
 
+	if(num < 0) { return "OOPS"; }
+	
+	arr.push(lastNum);
+	for(let i = 0; i <= num; i++) {
+		sum = lastNum + a;
+		a = lastNum;
+		lastNum = sum;
+		arr.push(sum);
+	}
+
+  return arr[num];
 };
 
 // Do not edit below this line
