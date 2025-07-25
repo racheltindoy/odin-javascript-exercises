5e38165bb603ed32f1ea220328c279166799f321
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-25 13:24:32 +0800

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (input) {
+	let reversedLetters = '';
+  	let originalLetters = '';
 
+	for(let i = input.length-1; i >= 0 ; i--) {
+	if (/[a-zA-Z0-9]/.test(input[i])) {
+	  reversedLetters += input[i].toLowerCase();
+	}
+	}
+
+  for(let i = 0; i <= input.length-1 ; i++) {
+	if (/[a-zA-Z0-9]/.test(input[i])) {
+	  originalLetters += input[i].toLowerCase();
+	}
+}
+
+  if(reversedLetters == originalLetters) {
+	return true;
+  } else {return false;}
 };
 
 // Do not edit below this line
